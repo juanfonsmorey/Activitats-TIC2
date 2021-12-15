3d889c9d365546e0b3c0bcdae9da9c35c1724af3
juanfonsmorey <93371055+juanfonsmorey@users.noreply.github.com>
2021-12-15 21:58:01 +0100

    Add files via upload

---
+++ cerezaact.py
@@ -0,0 +1,9 @@
+#Juan Fons Morey
+
+print("A. La guinda del postre")
+print("B. La guinda del desierto")
+entrada_usuario = input("Una cereza es:"
+if entrada_usuario.upper() == "A":
+    print("Correcto!")
+else:
+    print("No es correcto")
