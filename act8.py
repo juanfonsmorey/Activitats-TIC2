030a6b3ad32de86744d439174220aafb34ade346
juanfonsmorey <93371055+juanfonsmorey@users.noreply.github.com>
2021-12-16 12:06:52 +0100

    Add files via upload

---
+++ act8.py
@@ -0,0 +1,10 @@
+#Juan Fons Morey
+#Phyton
+
+"""
+Este programa no funciona bien. ¿Por qué?
+"""
+
+x = input("Cómo estás hoy?")
+if x == "Feliz" or x == "Satisfecho":
+    print ("Me alegra oír eso!")
