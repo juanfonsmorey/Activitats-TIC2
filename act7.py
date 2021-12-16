e3fef64bd8dfd704815a7358452487b4162ba9c0
juanfonsmorey <93371055+juanfonsmorey@users.noreply.github.com>
2021-12-16 12:04:16 +0100

    Add files via upload

---
+++ act7.py
@@ -0,0 +1,13 @@
+#Juan Fons Morey
+#Phyton
+
+"""
+Hay cuatro errores en este código. Identifícalos.
+"""
+
+respuesta = print("¿Cuál es el nombre del compañero de Sherlock Holmes?")
+respuesta = input("Respuesta: ")
+if respuesta == "Watson":
+    print("Correcto!")
+else:
+    print("Incorrecto! Es Watson.")
