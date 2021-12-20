54bfde0116cebfba3d8871dc71ba1494c7ff1d38
juanfonsmorey <93371055+juanfonsmorey@users.noreply.github.com>
2021-12-20 10:31:52 +0100

    Add files via upload

---
+++ 4.6.py
@@ -0,0 +1,12 @@
+#Juan Fons Morey
+#Phyton
+
+"""
+Escribe un programa que imprima un número entero al azar entre 1 y 10 (ambos incluidos).
+"""
+
+print("Aquest programa et dira un número aleatori del 1 al 10")
+
+import random
+x = (random.randrange(1,11))
+print(x)
