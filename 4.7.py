b137d301b601167b6c96df8d796161d864e34e9f
juanfonsmorey <93371055+juanfonsmorey@users.noreply.github.com>
2021-12-20 10:46:28 +0100

    Add files via upload

---
+++ 4.7.py
@@ -0,0 +1,11 @@
+#Juan Fons Morey
+#Phyton
+
+"""
+Escribe un programa que imprima un número real (float) al azar entre 1 y 10 (ambos incluidos).
+No vayas a cometer el error de generar un número aleatorio entre 0 y 10, en lugar de hacerlo entre 1 y 10.
+"""
+
+import random
+x = round(random.uniform(1.00,10.00), 2)
+print(x)
