54e53c0d9fa8d8b923ed154094ae230aec4bdc26
juanfonsmorey <93371055+juanfonsmorey@users.noreply.github.com>
2021-12-20 10:17:38 +0100

    Add files via upload

---
+++ 4.5.py
@@ -0,0 +1,14 @@
+#Juan Fons Morey
+#Phyton
+
+"""
+Este programa contiene tres errores. Identifica cada uno de ellos.
+"""
+
+print("Este programa recibe tres números y devuelve la suma.")
+total = 0
+ 
+for i in range(3):
+    x = int(input("Introduce un número: "))
+    total += x
+print("El total es:",total)
