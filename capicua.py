2216a0ba80a5ea1ef2c3a3cde4fb8d1568281a48
juanfonsmorey <93371055+juanfonsmorey@users.noreply.github.com>
2022-01-27 11:27:38 +0100

    Add files via upload

---
+++ capicua.py
@@ -0,0 +1,9 @@
+#Juan Fons Morey
+#Phyton
+
+print("Introdueix una paraula qualsevol: ")
+paraula = input()
+if str(paraula) == str(paraula)[::-1] :
+    print("Capicua")
+else:
+    print("No Capicua")
